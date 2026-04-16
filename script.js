c51e980448c376dea0faf608e35e51aa16aa21cc
Drak-Team <nafeesanjum2741495@gmail.com>
2026-04-16 19:25:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function login() {
 function signup() {
   alert("Account Created!");
 }
+function submitDeposit() {
+  let file = document.getElementById("screenshot").files[0];
+
+  if(file){
+    let reader = new FileReader();
+
+    reader.onload = function(){
+      let data = reader.result;
+
+      // save in localStorage (temporary)
+      localStorage.setItem("depositProof", data);
+
+      document.getElementById("msg").innerText = "Deposit Submitted!";
+    }
+
+    reader.readAsDataURL(file);
+
+  } else {
+    alert("Upload screenshot first!");
+  }
+}
