08ecd3e435b8685eea1954cde440bbd328122ed2
Drak-Team <nafeesanjum2741495@gmail.com>
2026-04-16 08:22:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+let index = 0;
+
+setInterval(() => {
+  const slides = document.querySelector(".slides");
+  index++;
+  if (index > 2) index = 0;
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}, 2500);
+
 function copyNumber() {
   navigator.clipboard.writeText("03025736141");
   alert("Number Copied!");
 }
-
-// Auto slider
-let index = 0;
-setInterval(() => {
-  let slides = document.querySelector(".slides");
-  index = (index + 1) % 3;
-  slides.style.transform = `translateX(-${index * 100}%)`;
-}, 3000);
